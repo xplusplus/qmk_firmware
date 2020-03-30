8e2d547ce3c4bba5d31a5ef834dd8eaadb54ec7a
Lauren Harris <lauren.y.harris@outlook.com>
2020-03-29 20:36:30 -0700

    Jordan iris layout

---
+++ keyboards/keebio/iris/keymaps/codecoffeecode/config.h
@@ -0,0 +1,33 @@
+/*
+Copyright 2020 Lauren Harris <github.com/xplusplus>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#define EE_HANDS
+
+#define DEBOUNCE 5
+#define TAP_CODE_DELAY 10
+#define ENCODER_RESOLUTION 2
+
+#undef  RGBLED_NUM
+#define RGBLED_NUM 12
+
+#define RGBLIGHT_ANIMATIONS
+#define RGBLIGHT_HUE_STEP 8
+#define RGBLIGHT_SAT_STEP 8
+#define RGBLIGHT_VAL_STEP 8
+
